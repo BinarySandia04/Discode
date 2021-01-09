b1dd20e79473dda93038a48384177dd6eb346807
BuildTools <unconfigured@null.spigotmc.org>
2021-01-09 23:06:06 +0000

    Epic unfinished bot

--- bot.py
+++ bot.py
@@ -1,23 +1,192 @@
 import discord
 import string
+import asyncio
+import time
 import os
 
 # PARAMS
-VM_NUMBER = 8
+VM_MAX_NUMBER = 128
 MOUNT_DIRS = ["bin", "usr", "lib"]
-AUTH_ID = 
+ADMIN_ID = 342287101225598987 # Syndria#2417
+PREFIX = {"cpp": "```cpp\n//run", "py": "```py\n#run", "python": "```python\n#run"}
 ########
-
+SCREEN_PREFIX = "dragoconda"
+#######
 
 client = discord.Client()
+vm_assosiations = {}
+user_assosiations = {}
+
+channel_assosiations = {}
+
+def discordTrim(text):
+    if len(text) < 1994:
+        return "```" + text + "```"
+    msgFinal = "\n... and many more"
+    return "```" + text[:(1990 - len(msgFinal))] + msgFinal + "```"
+
+def pythonGetOutput(text, finished):
+    a = text.split("\n")
+    res = ""
+    if finished:
+        if len(a) < 9:
+            return res
+        else:
+            for i in range(5, len(a) - 3):
+                res += a[i] + '\n'
+    else:
+        for i in range(5, len(a)):
+            res += a[i]
+    return res
+
+# Fixa un usuari a un discord channel
+def setUserToDcChannel(user, channel):
+    channel_assosiations[user] = channel
+
+def freeUserFromDcChannel(user):
+    channel_assosiations.pop(user, None)
+
+# Assigna una screen i una VM a un usuari
+def assignVm(user):
+    # Busquem en totes les vm i ens quedem amb la primera None
+    if len(vm_assosiations) >= VM_MAX_NUMBER:
+        return -1
+    for i in range(len(vm_assosiations) + 1):
+        if not i in vm_assosiations:
+            createVm(str(i))
+            createScreen(str(i))
+
+            vm_assosiations[i] = user
+            user_assosiations[user] = i
+
+            return i
+    return -1
+
+# Borra la VM i la screen a un usuari
+def removeVm(user):
+    if user in user_assosiations:
+        i = user_assosiations[user]
+        destroyScreen(str(i))
+        destroyVm(str(i))
+
+        vm_assosiations.pop(i, None)
+        user_assosiations.pop(user, None)
+
+
+
+# Crea una screen con su log file y tambien pone a disposición una vm
+def destroyScreen(name):
+    execute("rm screen." + str(name) + ".log")
+    screenCommand(name, "quit")
+   
+def getScreenLog(name):
+    f = open("screen." + str(name) + ".log", "r")
+    res = f.read()
+    f.close()
+    return res
+
+def createScreen(name):
+    destroyScreen(name)
+    execute("screen -dmS " + SCREEN_PREFIX + str(name) + " -L -Logfile screen." + str(name) + ".log")
+    screenCommand(name, "logfile flush 0")
+
+def screenCommand(name, command):
+    execute("screen -X -S " + SCREEN_PREFIX + str(name) + " " + command)
+
+def executeScreen(name, command):
+    execute("screen -S " + SCREEN_PREFIX + str(name) + " -X stuff \"" + command + "\n\"")
+
+# Gets guild stats for the bot
+def getGuilds():
+    global client
+    
+    users = 0
+
+    n = len(client.guilds)
+    s = "I AM AT **" + str(n) + "** SERVERS:\n"
+    
+    for guild in client.guilds:
+        s += "**" + guild.name + "** " + str(guild.id) + " " + str(guild.member_count) + "\n"
+        users += guild.member_count
+    s += "In total there are " + str(users) + " users"
+
+    return s
 
 async def sendMessage(textFormatted, channel):
-    await channel.send(textFormatted)
+    return await channel.send(textFormatted)
+
+async def wait(i):
+    await asyncio.sleep(i)
+
+# Funcions core, text és el codi trimmed ja llest per passar-ho
+async def startCppProgram(msg, author, text):
+    channel = channel_assosiations[author]
+    await sendMessage("C++ is not supported yet!", channel)
+    freeUserFromDcChannel(author)
+
+async def startPythonProgram(msg, author, text):
+    channel = channel_assosiations[author]
+    
+    # Reservar una sessio per al nostre usuari:
+    vm_index = assignVm(author)
+    
+    if vm_index != -1: 
+        # Write python code in the vm
+        pythonCode = open("root" + str(vm_index) + "/main.py", "w")
+        pythonCode.write(text)
+        pythonCode.close()
+
+        # Enter chroot environment
+        executeScreen(str(vm_index), "sudo chroot --userspec=dragoconda root" + str(vm_index))
+        # Run python file
+        executeScreen(str(vm_index), "python3 main.py")
+        # Exit chroot environment
+        executeScreen(str(vm_index), "exit")
+        
+        statusMsg = await sendMessage("Running the code...", channel)
+
+        await wait(2)
+
+        # Get the result
+        result = discordTrim(pythonGetOutput(getScreenLog(str(vm_index)), True))
+        print("RES: " + result)
+
+        removeVm(author)
+        await statusMsg.edit(content="Done!")
+        await sendMessage(result, channel)
+    else:
+        await sendMessage("Sorry, there are no VMs free right now. Maybe the bot is oversaturated", channel)
+    freeUserFromDcChannel(author)
+
+async def react(msg, emoji):
+    await msg.add_reaction(emoji)
 
 @client.event
 async def on_message(msg):
     content = msg.content
-    await sendMessage(msg.author.id, msg.channel)
+    author = msg.author.id
+
+    if author == client.user.id:
+        return
+    
+    if content == "!servers":
+        if author == ADMIN_ID:
+            await sendMessage(getGuilds(), msg.channel)
+    
+    elif content.startswith(PREFIX["cpp"]):
+        # c++
+        if author in user_assosiations:
+            return
+
+        setUserToDcChannel(author, msg.channel)
+        await startCppProgram(msg, author, content[len(PREFIX["cpp"]):len(content)-3])
+    elif content.startswith(PREFIX["py"]) or content.startswith(PREFIX["python"]):
+        # python
+        if author in user_assosiations:
+            return
+
+        setUserToDcChannel(author, msg.channel)
+        await startPythonProgram(msg, author, content[len(PREFIX["py"]):len(content)-3])
 
 @client.event
 async def on_ready():
@@ -40,34 +209,32 @@ def execute(s):
     print(s)
     os.system(s)
 
-# Removes instances of mounted Vms
-def removeVms(n):
-    for i in range(n):
-        vmPath = './root' + str(i)
-        if os.path.isdir(vmPath):
-            # Umount and remove
-            for d in MOUNT_DIRS:
-                execute('sudo umount ' + vmPath + "/" + d)
-            execute('sudo rm -R ' + vmPath)
-    return
+def destroyVm(n):
+    vmPath = './root' + str(n)
+    if os.path.isdir(vmPath):
+        # Umount and remove
+        for d in MOUNT_DIRS:
+            execute('sudo umount ' + vmPath + "/" + d)
+        execute('sudo rm -R ' + vmPath)
 
-# Creates mounted Vms
-def createVms(n):
-    for i in range(n):
-        vmPath = './root' + str(i)
-        if not os.path.isdir(vmPath):
-            execute('mkdir ' + vmPath + " " + vmPath + "/home")
-            for d in MOUNT_DIRS:
-                execute('mkdir ' + vmPath + "/" + d)
-                execute('sudo mount --bind ./root/' + d + " " + vmPath + "/" + d)
-    return
 
-def restartVMs(n):
-    print("Restarting VMs, this could take a while...")
-    removeVms(n)
-    createVms(n)
-    print("Successfully restarted VMs!")
-    return
+def createVm(n):
+    vmPath = './root' + str(n)
+    destroyVm(n)
+    
+    execute('mkdir ' + vmPath + " " + vmPath + "/home")
+    for d in MOUNT_DIRS:
+        execute('mkdir ' + vmPath + "/" + d)
+        execute('sudo mount --bind ./root/' + d + " " + vmPath + "/" + d)
 
-restartVMs(VM_NUMBER)
+async def some_function():
+    await asyncio.sleep(5)
+
+async def forever():
+    while True:
+        await some_function()
+
+
+############################################################
+asyncio.get_event_loop().create_task(forever())
 client.run(getToken())
